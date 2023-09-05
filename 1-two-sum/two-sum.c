4f2636579f175510e74ccf2768bce517826de899
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-09-05 19:35:26 +0530

    Time: 79 ms (65.14%) | Memory: 6.3 MB (79.47%) - LeetSync

---
+++ 1-two-sum/two-sum.c
@@ -0,0 +1,30 @@
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+
+
+    int i,j;
+    
+    int *arr=(int*)malloc(2*sizeof(int));
+   
+    
+
+    for(i=0;i<numsSize;i++)
+    {
+        for(j=i+1;j<numsSize;j++)
+        {
+            int total=nums[i]+nums[j];
+
+            if(total==target)
+            {
+                *returnSize=2;
+                
+                arr[0]=i;
+                arr[1]=j;
+                return arr;
+                
+            }
+        }
+    }
+    
+   return 0;
+
+}
