f0139e8e32ae9af3f9b3f16500b9be8d2db3ce34
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-09-09 22:26:56 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (24.61%) - LeetSync

--- 75-sort-colors/sort-colors.c
+++ 75-sort-colors/sort-colors.c
@@ -8,7 +8,7 @@ void swap(int *a,int *b)
 
 void sortColors(int* a, int n){
 
-    int i,j,start,index,end;
+  /*  int i,j,start,index,end;
     start=0;
     end=n-1;
     int mid=0;
@@ -32,5 +32,53 @@ void sortColors(int* a, int n){
 
         }
     }
+    */
+    int index=0,i,j,element;
+
+    if(n>1)
+    {
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==0 && index<n)
+        {
+            while(a[index]==0 && index<n-1)
+            {
+                index++;
+            }
+            if(index<i)
+                swap(&a[index],&a[i]);
+        }   
+    }
+    index=0;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==1 && index<n)
+        {
+            while((a[index]==1 || a[index]==0) && index<n-1)
+            {
+                index++;
+            }
+            if(index<i)
+                swap(&a[index],&a[i]);
+        }
+    }
+    index=0;
+
+   /* for(i=0;i<n;i++)
+    {
+        if(a[i]==2 && index<n)
+        {
+            while(a[index]==2 ||  && index<n-1)
+            {
+                index++;
+            }
+            if(index<i)
+                 swap(&a[index],&a[i]);
+        }
+    }*/
+    }
+    
+
+
 
 }
