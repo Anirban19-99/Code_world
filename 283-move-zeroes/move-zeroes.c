05065bb36c6da71a528fa8dd0aa059768e8a7a28
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-09-10 21:53:52 +0530

    Time: 164 ms (16.67%) | Memory: 15 MB (66.82%) - LeetSync

--- 283-move-zeroes/move-zeroes.c
+++ 283-move-zeroes/move-zeroes.c
@@ -1,52 +1,28 @@
-void moveZeroes(int* nums, int numsSize){
+void moveZeroes(int* a, int n){
 
-    int j, last;
-    int last_index=numsSize-1;
-    int n=numsSize;
-    int temp;
+  int i=0;
+  int next_non_zero=0;
 
+  for(i=0;i<n;i++)
+  {
+      if(a[i]==0)
+      {
+          next_non_zero=i;
+          while(a[next_non_zero]==0 && next_non_zero<n-1)
+          {
+              next_non_zero++;
+          }
+          if(next_non_zero<n)
+            swap(&a[i],&a[next_non_zero]);
+      }
 
-   /* for(i=0;i<n;i++)
-    {
-        if(nums[i]==0)
-        {
-            for(j=i;j<n-1;j++)
-            {
-                nums[j]=nums[j+1];
-            }
-            nums[last_index]=0;
-            last_index--;
-            n--;
-            i--;
+  }
 
-           
-        }
-    }
-    */
-    int i=0;
+}
 
-    for(i=0;i<n-1;i++)
-    {
-        if(nums[i]==0)
-        {
-           
-           int next_non_zero=i+1;
-           while(nums[next_non_zero]==0 && next_non_zero<n-1)
-                {
-                    next_non_zero++;
-                }
-          
-                
-            temp=nums[i];
-            nums[i]=nums[next_non_zero];
-            nums[next_non_zero]=temp;  
-                
-        
-           
-        }
-        
-       
-
-    }
-    
+void swap(int *a,int *b)
+{
+    int temp=*a;
+    *a=*b;
+    *b=temp;
 }
