89c4c06fb931244da6b41ea14202e0ecd155d1f8
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-09-10 22:00:11 +0530

    Time: 75 ms (56.00%) | Memory: 15.2 MB (35.12%) - LeetSync

--- 283-move-zeroes/move-zeroes.c
+++ 283-move-zeroes/move-zeroes.c
@@ -1,22 +1,33 @@
 void moveZeroes(int* a, int n){
 
   int i=0;
-  int next_non_zero=0;
+  
+
+  int zero=-1;
 
   for(i=0;i<n;i++)
   {
       if(a[i]==0)
       {
-          next_non_zero=i;
-          while(a[next_non_zero]==0 && next_non_zero<n-1)
-          {
-              next_non_zero++;
-          }
-          if(next_non_zero<n)
-            swap(&a[i],&a[next_non_zero]);
+         zero=i;
+         break;
       }
 
   }
+if(zero!=-1)
+{
+  for(i=zero+1;i<n;i++)
+  {
+      if(a[i]!=0)
+      {
+          swap(&a[i],&a[zero]);
+          zero++;
+      }
+      
+
+  }
+}
+
 
 }
 
