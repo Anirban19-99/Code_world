04ee1e7dfe1df7221587e08e1944a87578d7cf68
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-09-20 17:16:35 +0530

    Time: 4 ms (39.86%) | Memory: 7.7 MB (7.24%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,54 +1,37 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        // int n= a.size();
-        // int i,j;
-        // for(i=0;i<n;i++)
-        // {
-        //     for(j=i;j<n;j++)
-        //     {
-        //         // int temp=a[i][j];
-        //         // a[i][j]=a[j][i];
-        //         // a[j][i]=temp;
+    void rotate(vector<vector<int>>& a) {
+        int n= a.size();
+        int i,j;
+        for(i=0;i<n;i++)
+        {
+            for(j=i;j<n;j++)
+            {
+                // int temp=a[i][j];
+                // a[i][j]=a[j][i];
+                // a[j][i]=temp;
 
-        //         swap(a[i][j],a[j][i]);
-        //     }
-        // }
+                swap(a[i][j],a[j][i]);
+            }
+        }
 
-        // for(i=0;i<n;i++)
-        // {
-        //     reverse(a[i].begin(),a[i].end());
+        for(i=0;i<n;i++)
+        {
+            reverse(a[i].begin(),a[i].end());
 
-        //     // int u=0,v=n-1;
-        //     // while(u<v)
-        //     // {
-        //     //     int temp=a[i][v];
-        //     //     a[i][v]=a[i][u];
-        //     //     a[i][u]=temp; 
-        //     //      u++;
-        //     //     v--;
-        //     // }
+            // int u=0,v=n-1;
+            // while(u<v)
+            // {
+            //     int temp=a[i][v];
+            //     a[i][v]=a[i][u];
+            //     a[i][u]=temp; 
+            //      u++;
+            //     v--;
+            // }
            
 
 
-        // }
-
-
-        int n = matrix.size();
-    vector < vector < int >> rotated(n, vector < int > (n, 0));
-    for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-        rotated[j][n - i - 1] = matrix[i][j];
-    }
-    }
-
-    for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-         matrix[i][j]=rotated[i][j];
-    }
-    }
-
-
+        }
         
     }
 };
