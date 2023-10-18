ac5396703ffa98d202ef0904e7ebb7934d6c3146
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-10-18 08:48:47 -0700

    Time: 0 ms (100.00%) | Memory: 7.2 MB (52.82%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+
+        string ans;
+        int n=s.length();
+        vector<bool> taken(26,false);
+        vector<int> last_index(26);
+
+        for(int i=0;i<n;i++)
+        {
+            char ch=s[i];
+            last_index[ch-97]=i;
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            char ch=s[i];
+            int index=ch-97;
+            if(taken[index]==true)
+                continue;
+            
+            while(ans.length()>0 && ans.back()>ch && last_index[ans.back()-97]>i)
+            {
+                taken[ans.back()-97]=false;
+                ans.pop_back();
+            }
+
+            ans.push_back(ch);
+            taken[index]=true;
+
+
+
+        }
+
+        return ans;
+
+        
+    }
+};
