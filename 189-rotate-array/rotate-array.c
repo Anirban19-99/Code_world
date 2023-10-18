8b61d499fbc78661edf31c07c52b975551368f06
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-10-18 10:51:18 -0700

    Time: 101 ms (54.32%) | Memory: 23 MB (12.77%) - LeetSync

--- 189-rotate-array/rotate-array.c
+++ 189-rotate-array/rotate-array.c
@@ -1,57 +1,25 @@
-void rotate(int* nums, int numsSize, int k)
-{
-   int n=numsSize;
-   int a[n],i,j; 
+void rotate(int* a, int n, int k){
 
-   if(k<numsSize)
-   {
-   int index=n-k;
+    int ans[n];
+    k=k%n;
+    int start=n-k;
+    int j=0;
+    int i;
+    for(i=start;i<n;i++)
+    {
+        ans[j]=a[i];
+        j++;
+    }
 
-    if(index<0)
-        index=0-index;
+    for(i=0;i<start;i++)
+    {
+        ans[j]=a[i];
+        j++;
 
-   for( i=0;i<k;i++)
-   {
-       a[i]=nums[index];
-       index++;
-   }
-
-    j=0;
-
-   for( i=k;i<n;i++)
-   {
-       a[i]=nums[j];
-       j++;
-   }
-
-   for( i=0;i<n;i++)
-   {
-       nums[i]=a[i];
-       a[i]=NULL;
-   }
-   }
-   else
-   {
-        int t,t2,temp;
-        for(j=1;j<=k;j++)
-        {
-    
-            temp=nums[n-1];
-            t=nums[0];
-            for(i=1;i<n;i++)
-            {
-                t2=nums[i];
-                nums[i]=t;
-                t=t2;
-             }
-            nums[0]=temp;
-        }       
-
-   }
-   
-
-   
-
-   
+    }
+    for(i=0;i<n;i++)
+    {
+        a[i]=ans[i];
+    }
 
 }
