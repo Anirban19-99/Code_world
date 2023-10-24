613ea6224b0beb441472c93c19e5d4623f93f999
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-10-24 08:32:07 -0700

    Time: 1448 ms (5.58%) | Memory: 7.3 MB (47.78%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int n=s.length();
+        int n1=t.length();
+        if(n!=n1)
+            return false;
+
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(s[i]==s[j])
+                {
+                    if(t[i]==t[j])
+                    {
+
+                    }
+                    else
+                        return false;
+                }
+                else
+                {
+                    if(t[i]!=t[j])
+                    {
+
+                    }
+                    else
+                        return false;
+                }
+            }
+        }
+
+        return true;
+        
+    }
+};
