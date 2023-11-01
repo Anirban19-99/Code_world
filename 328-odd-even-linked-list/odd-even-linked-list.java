c5c489acf7b8d8d642f4af99d69b727e26e32659
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-11-01 21:58:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (66.26%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -13,56 +13,19 @@ class Solution {
 
         if(head==null || head.next==null)
             return head;
-
-        ListNode odd_head=head;
-        ListNode odd_tail=head;
-        ListNode even_head=null;
-        ListNode even_tail=null;
-        ListNode temp=head;
-        int length=0;
-        while(temp!=null)
+        ListNode odd=head;
+        ListNode even=head.next;
+        ListNode even_head=even;
+        while(odd.next!=null && even.next!=null)
         {
-            length++;
-            temp=temp.next;
+            odd.next=even.next;
+            odd=odd.next;
+            even.next=odd.next;
+            even=even.next;
         }
-        temp=head;
-        for(int i=1;i<=length;i++)
-        {
-            if(i%2==1)
-            {
-                if(i==1)
-                {
-                    odd_tail=head;
-                    odd_head=odd_tail;
-                }
-                else
-                {
-                    odd_tail.next=temp;
-                    odd_tail=temp;
-                }
-            }
-            else
-            {
-                if(i==2)
-                {
-                    even_head=temp;
-                    even_tail=even_head;
-                }
-                else
-                {
-                    even_tail.next=temp;
-                    even_tail=temp; 
-                }
+        odd.next=even_head;
 
-            }
-            if(i<length)
-                temp=temp.next;
-        }
-        odd_tail.next=even_head;
-        even_tail.next=null;
-
-        return odd_head;
-
+        return head;
         
     }
 }
