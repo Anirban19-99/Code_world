4b35f2882df52badcff93ae62c3f2e75732779c0
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-11-11 21:54:31 +0530

    Time: 6 ms (5.19%) | Memory: 43.7 MB (31.70%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int removeDuplicates(int[] a) {
+        ArrayList<Integer> ans= new ArrayList<>();
+        int i=0;
+        int count=1;
+        int n=a.length;
+        ans.add(a[0]);
+        for(i=1;i<n;i++)
+        {
+            if(a[i]==a[i-1])
+            {
+                if(count<2)
+                {
+                    ans.add(a[i]);
+                    count++;
+                }
+                else
+                {
+                    while(i<n && a[i]==a[i-1])
+                    {
+                        i++;
+                    }
+                    i--;
+                    System.out.println(a[i]);
+                    count=0;
+                }
+            }
+            else
+            {
+                count=1;
+                ans.add(a[i]);
+            }
+        }
+        for(i=0;i<ans.size();i++)
+        {
+            a[i]=ans.get(i);     
+        }
+
+        return ans.size();
+        
+    }
+}
