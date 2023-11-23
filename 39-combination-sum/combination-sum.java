a03f2f8c6c06283937d9126499c914616e3e0b8d
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-11-23 23:26:29 +0530

    Time: 6 ms (8.34%) | Memory: 44.1 MB (28.03%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,37 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] a, int target) {
+
+   // List<List<Integer>> final_ans= new ArrayList<>();
+   List < List < Integer >> final_ans = new ArrayList < > ();
+    List<Integer> ans= new ArrayList<>();
+    int index=0;
+    find_combination(a,ans,final_ans,target,index);
+    return final_ans;
+
+    
+        
+    }
+    int find_combination(int[] a,List<Integer> ans,List<List<Integer>> final_ans,int target, int index)
+    {
+        if(index==a.length)
+        {
+            if(target==0)
+            {
+                List<Integer> temp_ans= new ArrayList<>(ans);
+                final_ans.add(temp_ans);
+                System.out.println();
+                
+            }
+            return 0;
+        }
+
+        if(a[index]<=target)
+        {
+            ans.add(a[index]);
+            find_combination(a,ans,final_ans,target-a[index],index);
+            ans.remove(ans.size()-1);
+        }
+        find_combination(a,ans,final_ans,target,index+1);
+        return 0;
+    }
+}
