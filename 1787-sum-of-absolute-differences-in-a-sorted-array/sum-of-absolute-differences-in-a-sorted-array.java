8556156d853368d137d6a3eb8f4a299f95752351
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-11-26 20:03:12 +0530

    Time: 3 ms (99.37%) | Memory: 59.2 MB (45.91%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.java
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.java
@@ -1,29 +1,23 @@
 class Solution {
-    public int[] getSumAbsoluteDifferences(int[] a) {
-        int n=a.length;
-        int ans[]=new int[n];
-        int sum=0;
-        Map<Integer,Integer> sum_map= new HashMap<>();
-        for(int i=0;i<n;i++)
-        {
-            sum=sum+a[i];
-            sum_map.put(i,sum);
+    public int[] getSumAbsoluteDifferences(int[] nums) {
+        int sum = 0;
+        for (int n : nums) {
+            sum += n;
+        }
+        int left = 0;
+        int right = sum;
+
+        int[] r = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            right -= n;
+
+            r[i] = n * i - left + right - n * (nums.length - i - 1);
+
+            left += n;
         }
 
-        for(int i=0;i<n;i++)
-        { 
-            int first_half_sum;
-            if(i>0)
-                first_half_sum=(a[i]*i)-(sum_map.get(i-1));
-            else
-                first_half_sum=0;
-            int second_half_sum=(sum_map.get(n-1)-sum_map.get(i))-((n-1-i)*a[i]);
-            int res_ans=first_half_sum+second_half_sum;
-            a[i]=res_ans;
-        }
-
-        return a;
-
-        
+        return r;
     }
 }
