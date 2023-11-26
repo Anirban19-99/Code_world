4b1fcda83e14fe8f79201686453bc04e95adba30
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-11-26 21:00:00 +0530

    Time: 15 ms (83.84%) | Memory: 44.6 MB (49.72%) - LeetSync

---
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<Boolean> checkArithmeticSubarrays(int[] a, int[] l, int[] r) {
+
+        List<Boolean> final_ans=new ArrayList<>();
+        for(int i=0;i<l.length;i++)
+        {
+            int start_index=l[i];
+            int end_index=r[i];
+
+            int ar[]= new int[end_index-start_index+1];
+            int k=0;
+            for(int j=start_index;j<=end_index;j++)
+            {
+                ar[k]=a[j];
+                k++;
+            }
+            Arrays.sort(ar);
+            int diff=ar[1]-ar[0];
+            int check=0;
+            for(int m=0;m<ar.length-1;m++)
+            {
+                if(ar[m+1]-ar[m]==diff)
+                {
+
+                }
+                else
+                {
+                    check++;
+                    break;
+                }
+            }
+            if(check==0)
+                final_ans.add(true);
+            else
+                final_ans.add(false);
+
+        }
+
+        return final_ans;
+        
+    }
+}
