0c36412bbbeca1b48a6efa9994e251518494eae4
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-11-27 23:12:30 +0530

    Time: 7 ms (5.43%) | Memory: 42.7 MB (5.34%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n=nums.length;
+        List<Integer> cur= new ArrayList<>();
+        int cur_index=0;
+        generate_all(nums,cur_index,n,cur,ans);
+        return ans;
+        
+    }
+    static int generate_all(int[] a, int cur_index, int n,List<Integer> cur,List<List<Integer>> ans )
+    {
+        if(cur_index==n)
+        {
+            System.out.println(cur);
+            List<Integer> temp_ans= new ArrayList<>(cur);
+            ans.add(temp_ans);
+            return 0;
+        }
+        cur.add(a[cur_index]);
+        generate_all(a,cur_index+1,n,cur,ans);
+        cur.remove(cur.size()-1);
+        generate_all(a,cur_index+1,n,cur,ans);
+        return 1;
+
+    }
+}
