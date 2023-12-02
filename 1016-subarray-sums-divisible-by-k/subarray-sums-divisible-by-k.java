e8f2d7e8f974b2aa71caca20195a484801872472
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-12-02 12:19:26 +0530

    Time: 4 ms (83.97%) | Memory: 47.2 MB (11.02%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -1,30 +1,19 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        // Initialize a HashMap to store the count of remainders
-        Map<Integer, Integer> remainderCount = new HashMap<>();
-        remainderCount.put(0, 1); // Initialize with 0 to cover subarrays starting from index 0
-        
+        int[] remainderCount = new int[k];
+        remainderCount[0] = 1; // Initialize with 0 to cover subarrays starting from index 0
+
         int sum = 0;
         int count = 0;
 
-        // Iterate through the array to calculate the running sum and count subarrays
         for (int num : nums) {
             sum = (sum + num) % k;
             if (sum < 0) {
-                // Handle negative remainders
                 sum += k;
             }
 
-            // Check if there is a previous prefix sum with the same remainder
-            if (remainderCount.containsKey(sum)) {
-                count += remainderCount.get(sum);
-            }
-
-            // Update the count of the current remainder
-            remainderCount.put(sum, remainderCount.getOrDefault(sum, 0) + 1);
+            count += remainderCount[sum];
+            remainderCount[sum]++;
         }
 
         return count;
