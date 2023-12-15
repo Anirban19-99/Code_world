cbeb213d58488dc5e9f37be4bde75588848f4ab5
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-12-15 18:33:12 +0530

    Time: 1 ms (44.51%) | Memory: 40.6 MB (42.37%) - LeetSync

--- 551-student-attendance-record-i/student-attendance-record-i.java
+++ 551-student-attendance-record-i/student-attendance-record-i.java
@@ -1,15 +1,13 @@
 class Solution {
     public boolean checkRecord(String s) {
         int a_count=0;
+        String check="LLL";
+        if(s.contains(check))
+            return false;
         for(int i=0;i<s.length();i++)
         {
             if(s.charAt(i)=='A')
                 a_count++;
-            if(i<=s.length()-3)
-            {
-                if(s.charAt(i)=='L' && s.charAt(i+1)=='L' && s.charAt(i+2)=='L')
-                    return false;
-            }
         }
         if(a_count<2)
             return true;
