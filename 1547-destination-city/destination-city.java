9d14bfb6a2c07dcb54ec34dd667d2388957d2e03
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-12-15 18:16:20 +0530

    Time: 2 ms (79.52%) | Memory: 42.6 MB (92.62%) - LeetSync

--- 1547-destination-city/destination-city.java
+++ 1547-destination-city/destination-city.java
@@ -3,11 +3,7 @@ class Solution {
         Map<String, String> ans= new HashMap<>();
         for(int i=0;i<paths.size();i++)
         {
-            if(ans.containsKey(paths.get(i).get(0)))
-            {
-
-            }
-            else
+            if(!ans.containsKey(paths.get(i).get(0)))
             {
                 if(paths.get(i).size()==1)
                 {
@@ -19,18 +15,11 @@ class Solution {
 
         for(int i=0;i<paths.size();i++)
         {
-            if(ans.containsKey(paths.get(i).get(1)))
-            {
-
-            }
-            else
-            {
+            if(!ans.containsKey(paths.get(i).get(1)))
                 return paths.get(i).get(1);
-            }
         }
 
         int length1=paths.size();
-        int length2=paths.get(length1-1).size();
-        return paths.get(length1-1).get(length2-1);
+        return paths.get(paths.size()-1).get(paths.get(length1-1).size()-1);
     }
 }
