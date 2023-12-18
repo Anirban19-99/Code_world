f7cd5a869997effe67c9ab81e4c851682cef3810
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-12-18 20:34:38 +0530

    Time: 11 ms (8.40%) | Memory: 45.3 MB (6.62%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
@@ -1,8 +1,40 @@
 class Solution {
     public int maxProductDifference(int[] a) {
-        Arrays.sort(a);
+        //Arrays.sort(a);
+        int max1=Integer.MIN_VALUE;
+        int max2=Integer.MIN_VALUE;
+        int min1=Integer.MAX_VALUE;
+        int min2=Integer.MAX_VALUE;
+        int max_index=0;
+        int min_index=0;
         int n=a.length;
-        return (a[n-1]*a[n-2]-a[0]*a[1]);
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]>max1)
+            {
+                max2=max1;
+                max1=a[i];
+            }
+            else if(a[i]>max2)
+            {
+                max2=a[i];
+            }
+
+            if(a[i]<min1)
+            {
+                min2=min1;
+                min1=a[i];
+            }
+            else if(a[i]<min2)
+            {
+                min2=a[i];
+            }
+            
+        }
+        System.out.println(max1+","+max2);
+        System.out.println(min1+","+min2);
+
+        return max1*max2-min1*min2;
         
     }
 }
