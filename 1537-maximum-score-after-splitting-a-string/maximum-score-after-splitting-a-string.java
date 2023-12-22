ea70c2b91690845b05593c785e0baa9eb37d78df
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-12-22 23:16:33 +0530

    Time: 30 ms (9.33%) | Memory: 42.8 MB (31.56%) - LeetSync

---
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maxScore(String s) {
+        int total_sum=0;
+        int final_sum=0;
+        for(int i=0;i<s.length();i++)
+        {
+            int n=s.charAt(i)-'0';
+            total_sum=n+total_sum;
+        }
+        int prev_zero=0;
+        int prev_one=0;
+        int temp_sum=0;
+        if(s.charAt(0)=='1')
+        {
+            prev_one=1;
+            temp_sum=total_sum-1;
+            
+        }
+        else
+            prev_zero++;
+        for(int i=1;i<s.length();i++)
+        {
+            int n=s.charAt(i)-'0';
+            System.out.println(prev_zero+","+prev_one);
+            int rem_sum=total_sum-prev_one;
+            int sum=prev_zero+rem_sum;
+            if(sum>final_sum)
+                final_sum=sum;
+            if(n==1)
+            {
+                prev_one++;
+            }
+            else
+            {
+                prev_zero++;
+            }
+            
+
+        }
+        return Math.max(final_sum,temp_sum);
+    }
+}
