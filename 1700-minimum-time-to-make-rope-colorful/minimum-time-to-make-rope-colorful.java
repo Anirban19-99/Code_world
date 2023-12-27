c99f7ff6819965d5d89f9adcbe8e1de62db4d47c
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-12-27 22:40:30 +0530

    Time: 685 ms (5.14%) | Memory: 60.8 MB (15.68%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
@@ -1,34 +1,34 @@
 class Solution {
     public int minCost(String colors, int[] neededTime) {
         int time=0;
-        for(int i=0;i<colors.length()-1;i++)
+        int starting_index=-1;
+        int ending_index=-1;
+        int max=0;
+        for(int i=1;i<colors.length();i++)
         {
-            if(colors.charAt(i)==colors.charAt(i+1))
-            {
-                int starting_index=i;
-                int j=i;
-                int max=0;
-                int second_max=0;
-                while(colors.charAt(j)==colors.charAt(i))
-                {
-                    time=time+neededTime[j];
-                    if(max<neededTime[j])
-                    {
-                        second_max=max;
-                        max=neededTime[j];
-                    }       
-                    if(j==colors.length()-1)
-                        break;
-                    if(colors.charAt(j)==colors.charAt(j+1))
-                    {
-                        j++;
-                    }
-                    else
-                        break;
-                }
+           if(colors.charAt(i)==colors.charAt(i-1))
+           {
+               if(starting_index==-1)
+               {
+                   starting_index=i-1;
+                   time=time+neededTime[i-1];
+                   max=neededTime[i-1];
+               }
+               time=time+neededTime[i];
+               if(max<neededTime[i])
+               {
+                   max=neededTime[i];
+               }
+           }
+           if(colors.charAt(i)!=colors.charAt(i-1) || i==colors.length()-1)
+           {
+                System.out.println(time+","+max);
                 time=time-max;
-                i=j;
-            }
+                max=0;
+                starting_index=-1;
+           }
+
+           
         }
 
         return time;
