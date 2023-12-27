10852d79f4aa8e8f240c94e229645c6e0c1fc8ff
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-12-27 20:38:21 +0530

    Time: 10 ms (42.67%) | Memory: 60.5 MB (19.79%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
@@ -5,25 +5,30 @@ class Solution {
         {
             if(colors.charAt(i)==colors.charAt(i+1))
             {
-               int j=i;
-               while(colors.charAt(i)==colors.charAt(j))
+                int starting_index=i;
+                int j=i;
+                while(colors.charAt(j)==colors.charAt(i))
                 {
                     if(j==colors.length()-1)
                         break;
                     j++;
                 }
                 if(colors.charAt(i)!=colors.charAt(j))
-                    j--;
-                int max=0;
-                for(int k=i;k<=j;k++)
                 {
-                    time=time+neededTime[k];
-                    if(max<neededTime[k])
-                        max=neededTime[k];
+                    j--;
                 }
-                time-=max;
+                int max=0;
+                for(int k=starting_index;k<=j;k++)
+                {
+                    if(neededTime[k]>max)
+                    {
+                        max=neededTime[k];
+                    }
+                    time=time+neededTime[k];
+                }
+                time=time-max;
+
                 i=j;
-                
             }
         }
 
