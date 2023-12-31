27910689ca4c0c74d14144a0300e0c19db852b93
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-12-31 19:09:46 +0530

    Time: 4 ms (32.33%) | Memory: 41.5 MB (11.53%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.java
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.java
@@ -8,17 +8,14 @@ class Solution {
         for(int i=0;i<s.length();i++)
         {
             map.put(s.charAt(i),i);
-           
         }
-        int max_len=Integer.MIN_VALUE;
+        int max_len=-1;
         for(int i=0;i<s.length();i++)
         {
             int len=map.get(s.charAt(i))-i-1;
             if(len>max_len)
                 max_len=len;
         }
-        if(max_len<0)
-            max_len=-1;
         return max_len;
 
         
