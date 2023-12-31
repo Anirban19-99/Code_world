09d3a2754c5f05c06fd38b3c4dc045451f8211f2
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2023-12-31 19:09:00 +0530

    Time: 3 ms (37.34%) | Memory: 41.2 MB (16.04%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.java
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.java
@@ -4,32 +4,18 @@ class Solution {
         {
             return 0;
         }
-        Map<Character, List<Integer>> map=new HashMap<>();
+        Map<Character, Integer> map=new HashMap<>();
         for(int i=0;i<s.length();i++)
         {
-            if(map.containsKey(s.charAt(i)))
-            {
-                map.get(s.charAt(i)).add(i);
-            }
-            else
-            {
-                map.put(s.charAt(i),new ArrayList<>());
-                map.get(s.charAt(i)).add(i);
-            }
+            map.put(s.charAt(i),i);
+           
         }
         int max_len=Integer.MIN_VALUE;
         for(int i=0;i<s.length();i++)
         {
-            int len=map.get(s.charAt(i)).size();
-            if(len>=2)
-            {
-                int first_index=map.get(s.charAt(i)).get(0);
-                int last_index=map.get(s.charAt(i)).get(len-1);
-                if(last_index-first_index-1>max_len)
-                {
-                    max_len=last_index-first_index-1;
-                }
-            }
+            int len=map.get(s.charAt(i))-i-1;
+            if(len>max_len)
+                max_len=len;
         }
         if(max_len<0)
             max_len=-1;
