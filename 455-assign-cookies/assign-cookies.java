6ca92f69cac44e6f80fb62010f8c8112e9706502
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2024-01-01 21:11:28 +0530

    Time: 13 ms (8.99%) | Memory: 45.5 MB (6.13%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -5,9 +5,7 @@ class Solution {
         if(g.length==0 || s.length==0)
             return 0;
         int count=0;
-        if(g.length>s.length)
-        {
-            System.out.println("ok1");
+         System.out.println("ok");
             int i=0;//for g
             int j=0;//for s
 
@@ -29,33 +27,6 @@ class Solution {
                }
                 
             }
-        }
-        else
-        {
-            System.out.println("ok2");
-            int i=0;//for g
-            int j=0;//for s
-
-            while(g[0]>s[j])
-            {
-                j++;
-            }
-            while(j<s.length && i<g.length)
-            {
-               if(s[j]>=g[i])
-               {
-                   j++;
-                   i++;
-                   count++;
-               }
-               else
-               {
-                   j++;
-               }
-                
-            }
-        }
-
         return count;
         
     }
