c846af294de49a079856f4b7a8d39ed21d194cea
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2024-01-02 20:02:53 +0530

    Time: 14 ms (5.30%) | Memory: 45 MB (14.62%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,23 +1,40 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] a) {
         List<List<Integer>> ans= new ArrayList<>();
+        Map<Integer,Integer> map=new HashMap<>();
         List<Integer> array=new ArrayList<>();
         for(int i=0;i<a.length;i++)
         {
             array.add(a[i]);
+            if(map.containsKey(a[i]))
+            {
+                map.put(a[i],map.get(a[i])+1);
+            }
+            else
+            {
+                map.put(a[i],1);
+            }
         }
 
-        while(array.size()!=0)
+
+        while(map.size()!=0)
         {
-            List<Integer> temp=new ArrayList<>();
-            for(int i=0;i<array.size();i++)
+            List<Integer> temp= new ArrayList<>();
+            for(int i=0;i<a.length;i++)
             {
-                if(!temp.contains(array.get(i)))
+                if(!temp.contains(a[i]) && map.containsKey(a[i]))
                 {
-                    temp.add(array.get(i));
-                    array.remove(i);
-                    i--;
+                    temp.add(a[i]);
+                    if(map.get(a[i])==1)
+                    {
+                        map.remove(a[i]);
+                    }
+                    else
+                    {
+                        map.put(a[i],map.get(a[i])-1);
+                    }
                 }
+                
             }
             ans.add(temp);
         }
