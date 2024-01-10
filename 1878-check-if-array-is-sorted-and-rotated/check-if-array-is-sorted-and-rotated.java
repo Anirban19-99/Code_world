ef27c38b5cb9361643a3c0b3802e451efbcf6550
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2024-01-10 23:11:00 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (45.51%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean check(int[] a) {
         int flag=0;
-        int check_all=1;
+        int is_rotated=1;
         for(int i=0;i<a.length-1;i++)
         {
             if(a[i]>a[i+1])
             {
-                if(check_all==0)
+                if(is_rotated==0)
                     return false;
-                check_all=0;
+                is_rotated=0;
                 flag=i+1;
 
             }
         }
-        if(check_all==1)
+        if(is_rotated==1)
             return true;
         else
         {
