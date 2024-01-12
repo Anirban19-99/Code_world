b942c1a1edcd0df955346e792975ae600a358892
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2024-01-12 21:34:09 +0530

    Time: 1 ms (20.66%) | Memory: 45.3 MB (8.12%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,20 +1,25 @@
 class Solution {
-    public int[] shuffle(int[] a, int n) {
-        int[] ans= new int[a.length];
-        int i=0;
-        int j=n;
-        int k=0;
-        while(j<a.length)
-        {
-            ans[k]=a[i];
-            ans[k+1]=a[j];
-            k=k+2;
-            i++;
-            j++;
-        }
-
-
-        return ans;
-
+    public int[] shuffle(int[] nums, int n) {
+        int size=nums.length;
+	int[] a1= new int[n];
+	int[] finalArray= new int[size];
+	for(int i=0;i<n;i++){
+	    a1[i]=nums[i];
+	    
+	}
+int i = 0;
+int j = n;
+int k=0;
+while (k < finalArray.length && i < n) {
+    finalArray[k++] = a1[i++];
+    if (k < finalArray.length && j < nums.length) {
+        finalArray[k++] = nums[j++];
+    }
+}
+	      
+	       
+	       
+	       
+	return finalArray;
     }
 }
