1f860600434674c58722396030153a98ed58a40f
Sk Anirban <107425605+Anirban19-99@users.noreply.github.com>
2024-01-16 21:30:47 +0530

    Time: 9 ms (7.59%) | Memory: 44.2 MB (59.30%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,33 +1,37 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> currentSubset = new ArrayList<>();
+    public List<List<Integer>> combinationSum(int[] a, int target) {
 
-        findCombination(0, target, candidates, currentSubset, result);
-        return result;
-    }
+   // List<List<Integer>> final_ans= new ArrayList<>();
+   List < List < Integer >> final_ans = new ArrayList < > ();
+    List<Integer> ans= new ArrayList<>();
+    int index=0;
+    find_combination(a,ans,final_ans,target,index);
+    return final_ans;
 
-    public static void findCombination(int index, int target, int[] candidates, List<Integer> currentSubset, List<List<Integer>> result) {
-        // checks if we have explored all the elements of array
-        if(index == candidates.length) {
-            if(target == 0) {
-                result.add(new ArrayList<>(currentSubset));
-            }
-            return;
-        }
-
-        if(candidates[index] <= target) {
-            currentSubset.add(candidates[index]);
-            
-            // After adding the element of curr index, iterate the left path until the base condition is met
-            findCombination(index, target - candidates[index], candidates, currentSubset, result);
+    
         
-            // this is required because when the above recursion call 
-            // is executed then the Data structure still has curr index element so we need to remove it
-            currentSubset.remove(currentSubset.size() - 1);
+    }
+    int find_combination(int[] a,List<Integer> ans,List<List<Integer>> final_ans,int target, int index)
+    {
+        if(index==a.length)
+        {
+            if(target==0)
+            {
+                List<Integer> temp_ans= new ArrayList<>(ans);
+                final_ans.add(temp_ans);
+                System.out.println();
+                
+            }
+            return 0;
         }
 
-        // check for the next element of array
-        findCombination(index + 1, target, candidates, currentSubset, result);
+        if(a[index]<=target)
+        {
+            ans.add(a[index]);
+            find_combination(a,ans,final_ans,target-a[index],index);
+            ans.remove(ans.size()-1);
+        }
+        find_combination(a,ans,final_ans,target,index+1);
+        return 0;
     }
 }
